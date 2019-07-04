18b8e8a947c17ba8ada4156434aae7c7c52a30fe
Jason Belk <jason.belk@networktocode.com>
2019-07-04 14:30:53 -0700

    got rosetta to file working

--- demo-basic-nornir.py
+++ demo-basic-nornir.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-# thanks to Nick R example:
-# https://github.com/nickrusso42518/ansible2nornir/tree/master/getter/nornir
+# thanks to Nick R and dmfigol examples:
+
 """
 Nornir runbook to run arbitrary commands on network devices
 """
@@ -12,9 +12,12 @@ from nornir.core.task import Task
 from nornir.plugins.tasks.networking import napalm_cli
 from nornir.plugins.tasks.files import write_file
 from nornir.plugins.functions.text import print_result
+from nornir.core.filter import F
+from nornir.plugins.tasks.networking import napalm_get
 
 # rest api
 import json
+import yaml
 import requests
 from requests.exceptions import HTTPError
 from requests.auth import HTTPBasicAuth
@@ -28,15 +31,6 @@ headers = {
 # rosetta / yangify
 from ntc_rosetta import get_driver
 from yangson.exceptions import SemanticError
-
-def parse_config(task):
-    ios = get_driver("ios", "openconfig")
-    ios_driver = ios()
-    with open("data/ios/config.txt", "r") as f:
-        config = f.read()
-    parsed = ios_driver.parse(native={"dev_conf": config})
-    print("printing ntc_rosetta parsed config")
-    print(json.dumps(parsed.raw_value(), indent=4))
 
 def get_restconf_single_interface(task, model="native", port="2225",int_id="3"):
     if model == "native":
@@ -62,14 +56,72 @@ def get_restconf_all_interfaces(task, model="native", port="2225"):
     response = requests.get(url, headers=headers, auth=auth, verify=False)
     return response.text
 
+
+def native_config_from_disk(task):
+    with open("config/backed_up_from_device/{}_napalm_backup.conf".format(task.host.name), "r") as f:
+        native_config = f.read()
+    return native_config
+
+def rosetta_parse_native_to_data_model(task):
+    ios = get_driver("ios", "openconfig")
+    ios_driver = ios()
+    ntc_rosetta_data = ios_driver.parse(native={"dev_conf": native_config_from_disk(task)})
+    url = 'https://127.0.0.1:2225/restconf/data/Cisco-IOS-XE-native:native/interface'
+    response = requests.get(url, headers=headers, auth=auth, verify=False)
+    device_api = json.loads(response.text)
+    device_api = yaml.dump(device_api, default_flow_style=False)
+    ntc_rosetta_json = ntc_rosetta_data.raw_value()
+    ntc_rosetta_yaml = yaml.dump(ntc_rosetta_data.raw_value(), default_flow_style=False)
+    return ntc_rosetta_json, ntc_rosetta_yaml
+
+def backup(task, path):
+    r = task.run(
+        task=napalm_get,
+        getters=["config"],
+        severity_level=logging.DEBUG,
+    )
+    task.run(
+        task=write_file,
+        filename=f"{path}/{task.host}_napalm_backup.conf",
+        content=r.result["config"]["running"],
+    )
+
+def parse_config_to_json(task, path):
+    ntc_rosetta_json, ntc_rosetta_yaml = rosetta_parse_native_to_data_model(task)
+    ntc_rosetta_json = json.dumps(ntc_rosetta_json)
+    task.run(
+        task=write_file,
+        filename=f"{path}/{task.host}_rosetta.json",
+        content=ntc_rosetta_json,
+    )
+    task.run(
+        task=write_file,
+        filename=f"{path}/{task.host}_rosetta.yml",
+        content=ntc_rosetta_yaml,
+    )
+
 def main() -> None:
     nornir = InitNornir()
     # Get Openconfig Interface Data from CSR1000v from Restconf
-    result = nornir.run(task=get_restconf_all_interfaces, model="openconfig")
-    # print_result(result["csr1kv"], severity_level=logging.INFO)
-    # Get Openconfig Interface Data parsed from native CLI on disk
-    result = nornir.run(task=parse_config)
+    ios_devices = nornir.filter(F(platform="ios"))
+    # print(ios_devices.inventory.hosts.get("csr1kv").get("candidate_config_model"))
+    result = ios_devices.run(task=backup, path="config/backed_up_from_device")
     print_result(result, severity_level=logging.INFO)
+
+    result = ios_devices.run(task=parse_config_to_json, path="config/backed_up_from_device")
+    print_result(result, severity_level=logging.INFO)
+
+
+    # result = ios_devices.run(task=native_config_from_disk("ios","config.txt"))
+    # print_result(result)
+
+    # result = ios_devices.run(task=get_restconf_all_interfaces, model="openconfig")
+    # print_result(result["csr1kv"], severity_level=logging.INFO)
+
+    # # Get Openconfig Interface Data parsed from native CLI on disk
+
+    # result = ios_devices.run(task=parse_config)
+    # print_result(result, severity_level=logging.INFO)
 
 
 if __name__ == "__main__":
